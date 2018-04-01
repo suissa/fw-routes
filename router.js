1a42336b2b8759adbb40da7fd14267599af90f2e
Jean Carlo Nascimento <jnascimento@gmail.com>
2018-03-31 23:06:58 -0300

    Update router.js

--- router.js
+++ router.js
@@ -1,8 +1,11 @@
-exports.get = function(route){
-    (req, res) => {
+exports.get = function(route, cb = (res, data) => res.write('cb data: ' + JSON.stringify(data)) ){
+    (req, res, next) => {
         if(req.method === 'GET' && req.url === route){
             console.log('rota criada :D');
             req.pipe(res);
+            const list = [1,2,3,4,5,6]
+            cb(res, list)
+            return res.end()
         }
     }
 }
