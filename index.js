5466c3ab0e8396a2af50bf16fdc0c9ae9da1dbeb
Suissa <jnascimento@gmail.com>
2018-04-03 02:20:22 -0300

    refatoradinha massaa

--- index.js
+++ index.js
@@ -1,71 +1,12 @@
-const http = require( 'http' )
-const querystring = require( 'querystring' )
+const http = require('http')
+const server = http.createServer()
 
-const teste =[ {
-  Name: 'Adelmo Junior',
-  Age: 17
-}]
-
-const ERRORS = require('./lib/errors')
-
-const {
-  sendJSON,
-  setResponseJSON,
-  setResponseSuccess,
-  getResponseCreateSuccess,
-  getResponseFindSuccess,
-  getResponseFindOneSuccess,
-} = require('./lib/helpers')
-
-const ModelCeate = (req, res) => {
-
-  let data = ''
-
-  req.on('data', (body) => {
-    data += body
-  })
-
-  req.on('end', () => sendJSON(
-    setResponseSuccess(res, setResponseJSON()),
-    getResponseCreateSuccess(data))
-  )
-  // return data
+const app = {
+  use: require('./lib')
 }
 
-const actions = ({
-  create: (req, res) => (_data) => { 
-    return ModelCeate(req, res)
-  },
-  find: (req, res) => (data) => { 
-    return sendJSON(
-      setResponseSuccess(res, setResponseJSON()),
-      getResponseFindOneSuccess(data))
-  },
-  findOne: (req, res) => (data) => {
-    return sendJSON(
-      setResponseSuccess(res, setResponseJSON()),
-      getResponseFindSuccess(data[0]))
-  },
-  update: (req, res) => (data) => { 
-    res.write(sendJSON(data)) 
-    res.end()
-  },
-  remove: (req, res) => (data) => { 
-    res.write(sendJSON(data)) 
-    res.end()
-  },
-})
-
-const router = {
-  use: require('./lib/use')(require('./routes'))
-}
-const server = http.createServer( ( req, res, next ) => {
-  // res.writeHead( 200, { 'Content-Type': 'application/json' } )
-} )
-
-server.on('request', (req, res) => {
-  router.use(req, res)
-})
+const routes = require('./routes')
+server.on('request', app.use(routes))
 
 server.listen( 3000, () => {
   console.log( 'Server rodando de boas :D' )
